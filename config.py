027e98dfbcb7cf6c917ce75306e330b0d1c20acd
MD Tauhid Anam <22-48183-2@student.aiub.edu>
2025-10-27 02:58:50 +0600

    Add configuration for environment variables and API

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+# Environment variables (Render setup)
+STORE_ID = os.getenv("STORE_ID", "roboh68fe89500b77e")
+STORE_PASS = os.getenv("STORE_PASS", "roboh68fe89500b77e@ssl")
+
+BASE_URL = "https://sandbox.sslcommerz.com/gwprocess/v4/api.php"
+CURRENCY = "BDT"
